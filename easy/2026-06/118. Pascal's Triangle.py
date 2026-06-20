0ded9d07c29dc0c80ce02f89c0ce304e3910ac89
TIROK547 <ilia.gotbi.2006@gmail.com>
2026-06-20 16:35:33 +0330

    added new problems for dp

--- easy/2026-06/118. Pascal's Triangle.py
+++ easy/2026-06/118. Pascal's Triangle.py
@@ -1,10 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         dp = []
-        for i in range(numRows):
+        for i in range(1, numRows):
             cur_row = [0] * i
             cur_row[-1], cur_row[0] = 1, 1
 
             dp.append(cur_row)
+        
+        for i in range(1, numRows):
+            for j in range(1, i):
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
         print(dp)
 
